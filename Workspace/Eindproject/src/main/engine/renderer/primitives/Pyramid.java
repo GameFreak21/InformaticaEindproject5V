554b77279e662503e23199d93f3caa9d9c7d17b4
GameFreak21 <GameFreak21@users.noreply.github.com>
2020-05-06 15:51:40 +0200

    errors web

--- Workspace/Eindproject/src/main/engine/renderer/primitives/Pyramid.java
+++ Workspace/Eindproject/src/main/engine/renderer/primitives/Pyramid.java
@@ -1,49 +1,49 @@
-package main.engine.renderer.primitives;
-
-import main.engine.math.Vector3;
-import main.engine.renderer.graphics.Mesh;
-import main.engine.renderer.graphics.Vertex;
-import main.engine.objects.GameObject;
-
-public class Pyramid {
-	Vector3 pos;
-	Vector3 rot;
-	Vector3 scale;
-	
-	public GameObject gameObject;
-	
-	Mesh mesh = new Mesh(
-			new Vertex[] {
-					//bottom
-					new Vertex(new Vector3(-0.5f, -0.5f, -0.5f), new Vector3(1.0f, 0.0f, 0.0f)),
-					new Vertex(new Vector3(-0.5f, -0.5f, 0.5f), new Vector3(0.0f, 1.0f, 0.0f)),
-					new Vertex(new Vector3(0.5f, -0.5f, -0.5f), new Vector3(0.0f, 0.0f, 1.0f)),
-					new Vertex(new Vector3(0.5f, -0.5f, 0.5f), new Vector3(0.5f, 0.0f, 0.5f)),
-					
-					//top
-					new Vertex(new Vector3(0.0f, 0.5f, 0.0f), new Vector3(1.0f, 0.0f, 0.0f))
-					
-			},
-		new int[] {
-				0, 1, 4,
-				0, 2, 4,
-				1, 3, 4,
-				2, 3, 4,
-				0, 1, 2,
-				2, 3, 1
-		});
-		
-	public Pyramid(Vector3 pos, Vector3 rot, Vector3 size) {
-		this.pos = pos;
-		this.scale = size;
-		this.rot = rot;
-		this.gameObject = new GameObject(this.pos, this.rot, this.scale, this.mesh);
-	}
-	
-	public Pyramid(Vector3 pos, float len, Vector3 rot) {
-			this.pos = pos;
-			this.scale = new Vector3(len, len, len);
-			this.rot = rot;
-			this.gameObject = new GameObject(this.pos, this.rot, this.scale, this.mesh);
-	}
-}
+//package main.engine.renderer.primitives;
+//
+//import main.engine.math.Vector3;
+//import main.engine.renderer.graphics.Mesh;
+//import main.engine.renderer.graphics.Vertex;
+//import main.engine.objects.GameObject;
+//
+//public class Pyramid {
+//	Vector3 pos;
+//	Vector3 rot;
+//	Vector3 scale;
+//	
+//	public GameObject gameObject;
+//	
+//	Mesh mesh = new Mesh(
+//			new Vertex[] {
+//					//bottom
+//					new Vertex(new Vector3(-0.5f, -0.5f, -0.5f), new Vector3(1.0f, 0.0f, 0.0f)),
+//					new Vertex(new Vector3(-0.5f, -0.5f, 0.5f), new Vector3(0.0f, 1.0f, 0.0f)),
+//					new Vertex(new Vector3(0.5f, -0.5f, -0.5f), new Vector3(0.0f, 0.0f, 1.0f)),
+//					new Vertex(new Vector3(0.5f, -0.5f, 0.5f), new Vector3(0.5f, 0.0f, 0.5f)),
+//					
+//					//top
+//					new Vertex(new Vector3(0.0f, 0.5f, 0.0f), new Vector3(1.0f, 0.0f, 0.0f))
+//					
+//			},
+//		new int[] {
+//				0, 1, 4,
+//				0, 2, 4,
+//				1, 3, 4,
+//				2, 3, 4,
+//				0, 1, 2,
+//				2, 3, 1
+//		});
+//		
+//	public Pyramid(Vector3 pos, Vector3 rot, Vector3 size) {
+//		this.pos = pos;
+//		this.scale = size;
+//		this.rot = rot;
+//		this.gameObject = new GameObject(this.pos, this.rot, this.scale, this.mesh);
+//	}
+//	
+//	public Pyramid(Vector3 pos, float len, Vector3 rot) {
+//			this.pos = pos;
+//			this.scale = new Vector3(len, len, len);
+//			this.rot = rot;
+//			this.gameObject = new GameObject(this.pos, this.rot, this.scale, this.mesh);
+//	}
+//}
--- Workspace/Eindproject/src/main/engine/renderer/primitives/Wedge.java
+++ Workspace/Eindproject/src/main/engine/renderer/primitives/Wedge.java
@@ -1,57 +1,57 @@
-package main.engine.renderer.primitives;
-
-import main.engine.math.Vector3;
-import main.engine.renderer.graphics.Mesh;
-import main.engine.renderer.graphics.Vertex;
-import main.engine.objects.GameObject;
-
-public class Wedge {
-	Vector3 pos;
-	Vector3 rot;
-	Vector3 scale;
-	public GameObject gameObject;
-	
-	Mesh mesh = new Mesh(
-		new Vertex[] {
-				//wide
-				new Vertex(new Vector3(-0.5f, -0.5f, -0.5f), new Vector3(1.0f, 0.0f, 0.0f)),
-				new Vertex(new Vector3(-0.5f, -0.5f, 0.5f), new Vector3(0.0f, 1.0f, 0.0f)),
-				new Vertex(new Vector3(-0.5f, 0.5f, -0.5f), new Vector3(0.0f, 0.0f, 1.0f)),
-				new Vertex(new Vector3(-0.5f, 0.5f, 0.5f), new Vector3(0.5f, 0.0f, 0.5f)),
-				
-				//small
-				new Vertex(new Vector3(0.5f, -0.5f, -0.5f), new Vector3(1.0f, 0.0f, 0.0f)),
-				new Vertex(new Vector3(0.5f, -0.5f, 0.5f), new Vector3(0.0f, 1.0f, 0.0f))
-				
-		},
-		new int[] {
-				//wide
-				0, 1, 2,
-				1, 2, 3,
-				
-				//wide-to-small-sides
-				0, 4, 2,
-				1, 5, 3,
-				
-				//wide-to-small-bottom
-				0, 4, 5,
-				0, 1, 5,
-				
-				//wide-to-small-top
-				2, 4, 5,
-				2, 3, 5
-		});
-	
-	public Wedge(Vector3 pos, Vector3 rot, Vector3 size) {
-		this.pos = pos;
-		this.scale = size;
-		this.rot = rot;
-		this.gameObject = new GameObject(this.pos, this.rot, this.scale, this.mesh);
-	}
-	public Wedge(Vector3 pos, float len, Vector3 rot) {
-		this.pos = pos;
-		this.scale = new Vector3(len, len, len);
-		this.rot = rot;
-		this.gameObject = new GameObject(this.pos, this.rot, this.scale, this.mesh);
-	}
-}
+//package main.engine.renderer.primitives;
+//
+//import main.engine.math.Vector3;
+//import main.engine.renderer.graphics.Mesh;
+//import main.engine.renderer.graphics.Vertex;
+//import main.engine.objects.GameObject;
+//
+//public class Wedge {
+//	Vector3 pos;
+//	Vector3 rot;
+//	Vector3 scale;
+//	public GameObject gameObject;
+//	
+//	Mesh mesh = new Mesh(
+//		new Vertex[] {
+//				//wide
+//				new Vertex(new Vector3(-0.5f, -0.5f, -0.5f), new Vector3(1.0f, 0.0f, 0.0f)),
+//				new Vertex(new Vector3(-0.5f, -0.5f, 0.5f), new Vector3(0.0f, 1.0f, 0.0f)),
+//				new Vertex(new Vector3(-0.5f, 0.5f, -0.5f), new Vector3(0.0f, 0.0f, 1.0f)),
+//				new Vertex(new Vector3(-0.5f, 0.5f, 0.5f), new Vector3(0.5f, 0.0f, 0.5f)),
+//				
+//				//small
+//				new Vertex(new Vector3(0.5f, -0.5f, -0.5f), new Vector3(1.0f, 0.0f, 0.0f)),
+//				new Vertex(new Vector3(0.5f, -0.5f, 0.5f), new Vector3(0.0f, 1.0f, 0.0f))
+//				
+//		},
+//		new int[] {
+//				//wide
+//				0, 1, 2,
+//				1, 2, 3,
+//				
+//				//wide-to-small-sides
+//				0, 4, 2,
+//				1, 5, 3,
+//				
+//				//wide-to-small-bottom
+//				0, 4, 5,
+//				0, 1, 5,
+//				
+//				//wide-to-small-top
+//				2, 4, 5,
+//				2, 3, 5
+//		});
+//	
+//	public Wedge(Vector3 pos, Vector3 rot, Vector3 size) {
+//		this.pos = pos;
+//		this.scale = size;
+//		this.rot = rot;
+//		this.gameObject = new GameObject(this.pos, this.rot, this.scale, this.mesh);
+//	}
+//	public Wedge(Vector3 pos, float len, Vector3 rot) {
+//		this.pos = pos;
+//		this.scale = new Vector3(len, len, len);
+//		this.rot = rot;
+//		this.gameObject = new GameObject(this.pos, this.rot, this.scale, this.mesh);
+//	}
+//}
